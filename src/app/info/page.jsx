faac8ae593bc5d24dbac318af2db8bc3e373b886
Josiah Woresha <josiahworesha19@gmail.com>
2025-06-21 09:04:08 +0300

    changed the info to focus on client

--- src/app/info/page.jsx
+++ src/app/info/page.jsx
@@ -10,7 +10,7 @@ const Info = () => {
                 <img src="/developer.jpg" alt="developer image" />
             </div>
             <div className="col">
-                <p>By day, I am a software engineer who turns caffeine into clean code. By night, I am the person who mutters “it worked on my machine” while frantically Googling error codes. With a keyboard as my wand and Stack Overflow as my spellbook, I specialize in vanquishing bugs, wrestling APIs into submission, and occasionally convincing CSS that yes, it should just center the div.</p>
+                <p>I'm a results-driven web developer focused on creating seamless, high-performance digital experiences that meet real client needs. I prioritize understanding the goals, challenges, and vision behind every project to deliver solutions that are not only functional but impactful. With a strong emphasis on performance, accessibility, and user experience, I bring ideas to life through thoughtful design, efficient development, and a commitment to long-term value. Every project is an opportunity to translate client objectives into smart, scalable digital products.</p>
             </div>
         </div>
         </ReactLenis>
