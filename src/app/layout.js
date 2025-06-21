0e92d109f1a6cf3c1ad2d81bd3dddc0ecd24de77
Josiah Woresha <josiahworesha19@gmail.com>
2025-06-21 15:32:58 +0300

    added vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ViewTransitions } from "next-view-transitions";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/next"
 
 import Nav from "./components/Nav";
 
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
     <ViewTransitions>
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Analytics />
         <Nav />
         {children}
               <Footer />
